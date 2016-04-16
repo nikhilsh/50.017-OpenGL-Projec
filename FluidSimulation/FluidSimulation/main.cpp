c90d762284a2c4e67646ad4857bdb3e28c8c75ea
Nikhil Sharma <me@nikhil.sh>
2016-04-16 23:15:00 +0800

    some shit

---
+++ FluidSimulation/FluidSimulation/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  FluidSimulation
+//
+//  Created by Nikhil Sharma on 16/4/16.
+//  Copyright © 2016 Nikhil Sharma. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
